9625d58c74505125ee04c89a82eb0e52083c3e56
Megaflinterino <steven.p.rudolf@outlook.de>
2024-12-10 17:19:06 +0100

    Added the Function to calculate the highest GC-Content and output it. And read the sequences out of a fasta file

--- computingGCcontent.py
+++ computingGCcontent.py
@@ -1,44 +1,19 @@
+from Bio import SeqIO
+from Bio.SeqUtils import gc_fraction
+#   print(seq_record.id)s
+#    print(repr(seq_record.seq))
+#    print(seq_record.seq)
+#    print(len(seq_record))
 
-s  = "AGGCCGGTATATGCGATCTG"
-s2 = "CGAAGTCCTCGGACTATGAA"
-s3 = "CTTGGATAGAATCCATCCAC"
-s4 = "GCGGTAATACTCAAGGTTCA"
+max_gc_content = 0.0
 
-l = len(s)
-l2 = len(s2)
-l3 =  len(s3)
-l4 =  len(s4)
-
-GCcont  = 0
-GCcont2 = 0
-GCcont3 = 0
-GCcont4 = 0
+#calculates the gc_content of a sequence and if it is bigger then at beginning 0.0 and later the following biggest max_gc_content it gets stored in that variable
+for seq_record in SeqIO.parse("/home/steven/Bioinformatik/RosalindTasks/computingGCcontent.fasta", "fasta"):
+    #Multiplication with 100 because the output number would be0,...
+    gc_content = gc_fraction(seq_record) * 100
+    if gc_content > max_gc_content:
+        max_gc_content = gc_content
+        max_gc_id = seq_record.id
 
 
-for i in s:
-    if i == "C":
-        GCcont = GCcont + 1
-    if i == "G":
-        GCcont = GCcont + 1
-
-for i in s2:
-    if i == "C":
-        GCcont2 = GCcont2 + 1
-    if i == "G":
-        GCcont2 = GCcont2 + 1
-
-for i in s3:
-    if i == "C":
-        GCcont3 = GCcont3 + 1
-    if i == "G":
-        GCcont3 = GCcont3 + 1
-
-for i in s4:
-    if i == "C":
-        GCcont4 = GCcont4 + 1
-    if i == "G":
-        GCcont4 = GCcont4 + 1
-
-print(l, l2, l3, l4)
-print(GCcont / l, GCcont2 / l2, GCcont3 / l3, GCcont4 / l4)
-print(max(GCcont/l, GCcont2/l, GCcont3/l, GCcont4/l))
+print(f"{max_gc_id}\n{max_gc_content:.6f}")
