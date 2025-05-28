b943e405d4e4eed373605e77b223f3e5948674d1
megaflinterino <steven.p.rudolf@outlook.de>
2025-05-28 14:58:25 +0200

    Functioning now

--- finding_a_motif_in_DNA.py
+++ finding_a_motif_in_DNA.py
@@ -1,4 +1,14 @@
 
-DNA = "GATATATGCATATACTT"
-motif = "ATAT"
+##DNA = "GATATATGCATATACTT"
+##motif = "ATAT"
 
+import re
+#motif = "ATAT"
+dna = "GATATATGCATATACTT"
+
+#for i in re.finditer(motif,dna):
+#    print(i.start()+1)
+
+for i in range(len(dna)):
+    if  dna[i:] .startswith("ATAT"):
+        print(i+1)
