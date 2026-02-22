7a4f62c8c6d0779de2f4828cf18f68617fd15cd0
megaflinterino <steven.p.rudolf@outlook.de>
2026-02-22 16:44:22 +0100

    finalized restructuring

--- TranslationTest.py
+++
@@ -1,7 +0,0 @@
-inputDNA = "Hello World"
-
-translationTable = str.maketrans('Hel', 'Ga1')
-     
-complementaryDNA = inputDNA.translate(translationTable)
-
-print(complementaryDNA)
--- complementingAstrandOfDNA.py
+++
@@ -1,9 +0,0 @@
-
-
-original_dna = "insert here DNA snippet"[::-1]
-
-translation_table = str.maketrans('ATCG', 'TAGC')
-     
-complementary_dna = original_dna.translate(translation_table)
-
-print(complementary_dna)
